01303e7f7d25732d440f84682a4a3211b5b2e822
Liar233 <liar233@gmail.com>
2018-09-17 21:58:36 +0300

    Json draft

--- api.go
+++ api.go
@@ -5,8 +5,16 @@ import (
 	"github.com/gorilla/mux"
 	"io"
 	"fmt"
-	"time"
+	"encoding/json"
+	"io/ioutil"
 )
+
+type EventDto struct {
+	ID       string `json:"id"`
+	Channel  string `json:"channel"`
+	Payload  string `json:"payload"`
+	FireTime string `json:"fireTime"`
+}
 
 type ApiController struct {
 	router *mux.Router
@@ -14,18 +22,19 @@ type ApiController struct {
 
 // get list api response
 func (api *ApiController) eventList(w http.ResponseWriter, r *http.Request) {
-	println("Route started")
-
-	io.WriteString(w, "Events list")
-
-	time.Sleep(time.Duration(5) * time.Second)
-
-	println("Route stopped")
+	io.WriteString(w, "Event list")
 }
 
 // create api
 func (api *ApiController) createEvent(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Create event")
+
+	eventDto := EventDto{}
+
+	if err := requestToDto(r, &eventDto); err != nil {
+		println(err.Error())
+	}
+
+	fmt.Printf("%+v\n", eventDto)
 }
 
 // get the event
@@ -40,6 +49,17 @@ func (api *ApiController) deleteEvent(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	io.WriteString(w, fmt.Sprintf("Delete event %s", vars["id"]))
+}
+
+func requestToDto(r *http.Request, dto interface{}) error {
+	var body []byte
+	var err error
+
+	if body, err = ioutil.ReadAll(r.Body); err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, dto)
 }
 
 func NewApiController() *ApiController {
